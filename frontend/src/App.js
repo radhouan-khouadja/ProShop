aa734a4fc470ff6ff1d93a6a6c0cbcae82ab764e
radhouan khouadja <radhouankhouadja@gmail.com>
2021-09-05 09:02:11 +0100

    product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/HeaderComponent";
 import Footer from "./components/FooterComponent";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   );
 };
 
--- frontend/src/components/ProductComponent.jsx
+++ frontend/src/components/ProductComponent.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import Rating from "../components/RatingComponent";
 
 const ProductComponent = ({ product }) => {
   return (
     <Card className="rounded my-3 p-2">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="div">
           <Rating
             value={product.rating}

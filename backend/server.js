7c1bd6db21d19d26ad6a711c617a2f3c9667dc6f
radhouan khouadja <radhouankhouadja@gmail.com>
2021-09-06 07:41:37 +0100

    database config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+connectDB();
+
 const app = express();
 
 app.get("/api/products", (req, res) => {

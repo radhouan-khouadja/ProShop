a7be82db765e8521382e36ff195786e8d1156f63
radhouan khouadja <radhouankhouadja@gmail.com>
2021-09-07 02:34:57 +0100

    Preparing Sample Data

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,24 @@
 import mongoose from "mongoose";
 
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
 const productSchema = mongoose.Schema(
   {
     user: {
